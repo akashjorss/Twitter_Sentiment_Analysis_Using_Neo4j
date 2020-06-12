36c79329527803f7c1334f3135002f86d2a5e80d
akashjorss <akash.jorss@gmail.com>
2020-06-12 03:28:05 +0200

    End of the day commit

--- spark-streaming.py
+++ spark-streaming.py
@@ -3,23 +3,55 @@ from pyspark.streaming import StreamingContext
 import pickle, json
 import utils
 from Neo4j import Neo4j
+from elastic import Elastic
+import os
 
 COMPANIES = ['google', 'microsoft', 'ibm', 'sap', 'amazon', 'accenture', 'bmw', 'siemens', 'nvidia', 'apple']
-if __name__ == "__main__":
-    # Create a local StreamingContext and batch interval of 1 second
+
+def create_spark_context(interval):
+    """
+    :param interval: in seconds. To create a local StreamingContext and batch interval of interval seconds
+    :return:
+    """
 
     sc = SparkContext("local[*]", "TwitterETL")
+    ssc = StreamingContext(sc, interval)
+    return ssc
 
-    ssc = StreamingContext(sc, 1)
+def load_to_elastic(tweets):
+    """
+    :param tweets: list of json objects
+    :return: None
+    """
+    # upload to elastic
+    # elastic = Elastic(cloud_id=os.environ['ELASTIC_CLOUD_ID'],
+    #                   username=os.environ['ELASTIC_USERNAME'], password=os.environ['ELASTIC_PASSWORD'])
+    # elastic.load_data(tweets, "tweet-index")
+    print("Here are the tweets for elastic")
+    print(tweets)
 
+
+def load_to_neo4j(tweets):
+    """
+    :param tweets: list of json object
+    :return: None
+    """
+    neo4j = Neo4j()
+    neo4j.bulk_load(tweets)
+
+
+def run_spark_job(ssc):
+    """
+    :param ssc: spark streaming context
+    :return:
+    """
     # get the tweets
     tweets = ssc.socketTextStream("localhost", 10001)
-
     # convert string data to json
     tweets_json = tweets.map(lambda x: json.loads(x))
 
     # filter data based if hashtags are present in tweet or not
-    filtered_tweets = tweets_json.filter(lambda x: len(x["entities"]["hashtags"]) > 0)
+    filtered_tweets = tweets_json #.filter(lambda x: len(x["entities"]["hashtags"]) > 0)
 
     # find out which companies are contained in the tweet
     company_tweet_pair = filtered_tweets.flatMap(lambda x: utils.identify_company(x, COMPANIES))
@@ -27,16 +59,37 @@ if __name__ == "__main__":
     # prune the the tweets
     pruned_tweets = company_tweet_pair.map(lambda x: utils.prune_tweet(x[1], x[0]))
 
+    # cache it because it will be used more than once
+    # pruned_tweets = pruned_tweets.cache()
+
+    # stream analytics
+    ssc.checkpoint("./checkpoints")
+    stream_analytics = pruned_tweets.map(lambda t: (t['company'], 1)) \
+                                     .reduceByKeyAndWindow(func=lambda x, y: x+y, invFunc=lambda x, y: x-y,
+                                                          windowDuration=300, slideDuration=5)
+
+    stream_analytics.foreachRDD(lambda rdd: load_to_elastic(dict({"company": t[0], "count": t[1]}) for t in rdd.collect())) #generating wierd result
+
     # upload to neo4j
-    neo4j = Neo4j()
-    pruned_tweets.foreachRDD(lambda rdd: neo4j.bulk_load(rdd.collect()))
+    pruned_tweets.foreachRDD(lambda rdd: load_to_neo4j(rdd.collect()))
 
-    pruned_tweets.pprint()
+    #pruned_tweets.pprint()
 
-    #start the spark streaming context
-    ssc.start()
-    ssc.awaitTermination()
 
+if __name__ == "__main__":
+
+    try:
+        ssc = create_spark_context(1)
+        run_spark_job(ssc)
+
+        #
+        #
+        # #start the spark streaming context
+        ssc.start()
+        ssc.awaitTermination()
+
+    finally:
+        ssc.stop()
 
 
 
